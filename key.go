2d98f8366410ade1735c2eb2ccdb92da1b75cc71
mtavano <chelotavano.udp@gmail.com>
2017-12-04 22:12:56 -0300

    Add key struct and KeyStore interface definition

--- key.go
+++ key.go
@@ -1,5 +1,22 @@
 package atlas
 
+import "time"
+
 // Key ...
 type Key struct {
+	ID     int    `db:"id"`
+	GateID int    `db:"gate_id"`
+	UserID int    `db:"user_id"`
+	Name   string `db:"name"`
+	Token  string `db:"token"`
+
+	ExpiredAt *time.Time `db:"expired_at"`
+	CreatedAt time.Time  `db:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at"`
+}
+
+// KeyStore ...
+type KeyStore interface {
+	// Returns a key by given token
+	FindKeyByToken(token string) (*Key, error)
 }
