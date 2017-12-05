6409c3810eb99e7ff40f6110297c67419c622473
mtavano <chelotavano.udp@gmail.com>
2017-12-04 22:14:24 -0300

    Add logic and database connection to auth service

--- service/auth.go
+++ service/auth.go
@@ -4,35 +4,38 @@ import (
 	"log"
 	"time"
 
+	"github.com/mtavano/atlas"
 	"github.com/paypal/gatt"
 	rpio "github.com/stianeikeland/go-rpio"
 )
 
 // NewAuthService ...
-func NewAuthService() *gatt.Service {
+func NewAuthService(kst atlas.KeyStore) *gatt.Service {
 	s := gatt.NewService(gatt.MustParseUUID("2000000F-3ED1-488D-B6E8-6A27D888E256"))
 	s.AddCharacteristic(gatt.MustParseUUID("2000001F-3ED1-488D-B6E8-6A27D888E256")).HandleWriteFunc(
 		func(r gatt.Request, data []byte) (status byte) {
-			key := string(data)
+			token := string(data)
 
-			if key == "development-key" {
-				log.Println("Opening door ...")
-
-				_ = rpio.Open()
-				defer rpio.Close()
-
-				relay := rpio.Pin(16)
-				relay.Output()
-
-				relay.High()
-				time.Sleep(3 * time.Second)
-				relay.Low()
-
-				return gatt.StatusSuccess
+			k, err := kst.FindKeyByToken(token)
+			if err != nil {
+				log.Println("Key: ", token)
+				log.Println("Incorrect key: ", data)
+				return gatt.StatusUnexpectedError
 			}
 
-			log.Println("Incorrect key: ", key)
-			return gatt.StatusUnexpectedError
+			log.Println("key struct: ", &k)
+
+			_ = rpio.Open()
+			defer rpio.Close()
+
+			relay := rpio.Pin(16)
+			relay.Output()
+
+			relay.High()
+			time.Sleep(3 * time.Second)
+			relay.Low()
+
+			return gatt.StatusSuccess
 		})
 	return s
 }
